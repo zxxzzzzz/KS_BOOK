b091afad08ecf3797964b9c9ba23bc29b1726684
zxxzzzzz <812603872@qq.com>
2017-12-19 13:15:59 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -5,8 +5,6 @@ CREATE DATABASE  `KS`
 CHARACTER SET 'utf8'
 COLLATE 'utf8_general_ci';
 USE `KS`;
-
--- ----------------该字段只能设置一个角色，去除了课设要求里的一人任多职的要求。
 -- ----------------
 DROP TABLE IF EXISTS `TBUser`;
 CREATE TABLE `TBUser`(
@@ -16,6 +14,7 @@ CREATE TABLE `TBUser`(
 `Role` VARCHAR(255) NOT NULL COMMENT '扮演的角色', -- --诸如：教师/学生/管理员
 `Online` VARCHAR(10) NOT NULL COMMENT '是否在线（yes/no）', -- --online表示在线，限制重复登录。当online=yes ,表示已有人登录该账号，不能再登。
 `BlackList` VARCHAR(10) NOT NULL COMMENT '是否在黑名单（yes/no）',-- --在黑名单会有功能限制
+`Score` INT(11) NOT NULL COMMENT '个人评分' -- --用来做是否加入黑名单依据  score<0进黑名单（-100-100）
 PRIMARY KEY (`ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- ----------------------------------------------------------------------------------------------------用户
@@ -53,13 +52,12 @@ CREATE TABLE `TBRoom`(
 PRIMARY KEY (`ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --不提供自主选座位功能，随机给个空座位
 DROP TABLE IF EXISTS `TBDesk`; -- --座位
 CREATE TABLE `TBDesk`(
 `ID` INT(11) NOT NULL AUTO_INCREMEN,
 `RoomID` INT(11) NOT NULL COMMENT'所属教室id',
-`State` varchar(11) NOT NULL COMMENT'桌子状态',-- --（using/leaving/emptying）
-`UserID` INT(11) NOT NULL COMMENT '使用者id',
+`State` varchar(11) NOT NULL COMMENT'桌子状态',-- --（using/leaving/emptying/ordering）
+`UserID` INT(11) NOT NULL COMMENT '使用者id', 
 PRIMARY KEY (`ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -70,10 +68,18 @@ CREATE TABLE `TBAgainstHistory`(
 `Time` DATETIME(255) NOT NULL COMMENT '违反时间',
 `Detail` varchar(255)  NOT NULL COMMENT '违反细节', -- --诸如："在学习时晒臭脚" "暂离后未在时间内回归"
 `DeskID` INT(11) NOT NULL COMMENT '违反者使用桌子id',
+`Score` INT(11) NOT NULL COMMENT '扣分'
 PRIMARY KEY (`ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+DROP TABLE IF EXISTS `TBOrderHistory`; -- --预约记录
+CREATE TABLE `TBOrderHistory`(
+`ID` INT(11) NOT NULL AUTO_INCREMEN,
+`UserID` INT(11) NOT NULL COMMENT '预约者id',
+`Time` DATETIME(255) NOT NULL COMMENT '预约时间',
+`DeskID` INT(11) NOT NULL COMMENT '预约桌子id',
+PRIMARY KEY (`ID`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
